a9b5f9ff91a176bb9e70f010c3d90ab03d733b60
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-08-06 17:12:21 +1200

    migrate from CRA to vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+/* eslint-enable import/no-extraneous-dependencies */
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+  },
+  build: {
+    outDir: './build',
+  },
+});
